84c81c2cb50aa992b41615ebbe4bb953f3964864
Your Name <you@example.com>
2024-06-10 11:19:02 +0200

    Restructured.

--- src/pythontemplate/__main__.py
+++ src/pythontemplate/__main__.py
@@ -1,16 +1,19 @@
 """Entry point for the project."""
+
 from typing import List
-from src.pythontemplate.get_local_json_data import website_to_json
 
+from src.pythontemplate.get_website_data.crawl_website import website_to_json
 
-company_urls:List[str] = ["https://weaviate.io/"]
-json_output_path:str = "weaviate.json"
-weaveate_local_host_url:str="http://localhost:8080"
+company_urls: List[str] = ["https://weaviate.io/"]
+json_output_path: str = "weaviate.json"
+weaveate_local_host_url: str = "http://localhost:8080"
 
 # Get the json data.
 # website_to_json(urls=company_urls, json_output_path=json_output_path)
-visited = set() # Initialise an empty website set.
-website_to_json(url=company_urls[0], output_file=json_output_path, visited=visited)
+visited = set()  # Initialise an empty website set.
+website_to_json(
+    url=company_urls[0], output_file=json_output_path, visited=visited
+)
 print(f"Crawled company website and stored results in:{json_output_path}")
 
 # Structure the json data
@@ -20,7 +23,7 @@ print(f"Crawled company website and stored results in:{json_output_path}")
 load_local_json_data_into_weaviate(json_input_path=json_output_path)
 
 # Limit the number of queries to summarise to 3.
-max_nr_of_queries:int = 3
+max_nr_of_queries: int = 3
 
 # Perform queries to summarise the data.
 summarised_data = ask_weaviate_to_summarise(json_input_path=json_output_path)
---
+++ src/pythontemplate/load_json_into_weaviate/import_local_json.py
@@ -0,0 +1,42 @@
+# Source: https://weaviate.io/developers/weaviate/tutorials/import
+
+import json
+
+import weaviate
+
+
+def load_local_json_data_into_weaviate(
+    *,
+    weaveate_local_host_url: str,
+    json_input_path: str,
+    json_type: str,
+    type_property: str,
+):
+    client = weaviate.Client(
+        url=weaveate_local_host_url,
+        # additional_headers={
+        # "X-OpenAI-Api-Key": "YOUR-OPENAI-API-KEY"  # Or "X-Cohere-Api-Key" or "X-HuggingFace-Api-Key"
+        # }
+    )
+
+    # Open the file in read mode
+    try:
+        with open(json_input_path) as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        print(f"Error: File '{json_input_path}' not found.")
+        exit()
+
+    # Prepare a batch process
+    client.batch.configure(batch_size=100)  # Configure batch
+    with client.batch as batch:
+
+        # Batch import all Questions
+        for i, d in enumerate(data):
+            if i < 3:
+                # TODO: verify the data element is not already in weaviate.
+                properties = {
+                    type_property: d["Answer"],
+                }
+
+                batch.add_data_object(properties, json_type)
